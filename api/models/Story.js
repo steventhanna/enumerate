20a19f6f9a92c51e8d96f3cdfb4eaac0557f0b0e
steventhanna <steventhanna@gmail.com>
2015-11-08 00:50:19 -0500

    Added story model

--- api/models/Story.js
+++ api/models/Story.js
@@ -1,14 +1,47 @@
 /**
-* Story.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Story.js
+ *
+ * @author Steven T Hanna http://steventhanna.github.com/steventhanna
+ * @description :: The model for the story
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
   attributes: {
 
+    sid: {
+      type: 'string',
+      unique: true,
+      required: true
+    },
+
+    title: {
+      type: 'string',
+      required: true
+    },
+
+    contents: {
+      type: 'string',
+      required: true
+    },
+
+    // Include?
+    images: {
+      type: 'array'
+    },
+
+    tags: {
+      type: 'array'
+    },
+
+    views: {
+      type: 'float',
+    },
+
+    author: {
+      type: 'string',
+    },
+
   }
 };
-
