547a8ff2604de00d851d7723e08de0649b9f6ecf
steventhanna <steventhanna@gmail.com>
2015-11-08 01:23:23 -0500

    Beginning of enumerate class

--- api/controllers/EnumerateController.js
+++ api/controllers/EnumerateController.js
@@ -6,6 +6,28 @@
  */
 
 module.exports = {
-	
-};
 
+
+  new: function(req, res) {
+
+    var data = {
+      storyList: [],
+      tags: [],
+      popularStack: []
+    };
+
+    Enumerate.create(data).exec(function(err, enumer) {
+      if (err || enumer == undefined) {
+        console.log("There was an error creating the enumerate.");
+        console.log("Error = " + err);
+        console.log("Error Code: 00005");
+      } else {
+        // Enmerate type created
+        res.send({
+          success: true,
+          status: 200
+        });
+      }
+    });
+  },
+};
