3f8b5f16e8550004751f89119ebc8f0289e4997c
steventhanna <steventhanna@gmail.com>
2015-11-08 00:46:22 -0500

    Added model for tag

--- api/models/Tag.js
+++ api/models/Tag.js
@@ -1,14 +1,29 @@
 /**
-* Tag.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Tag.js
+ *
+ * @author Steven T Hanna http://steventhanna.github.com/steventhanna
+ * @description :: The model for the tag.  Lookup by name, not ID
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
   attributes: {
 
+    connections: {
+      type: 'array',
+      required: true
+    },
+
+    name: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+
+    color: {
+      type: 'string',
+    },
+
   }
 };
-
